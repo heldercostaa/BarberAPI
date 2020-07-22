5459b9a190f8716ad52b50d476ae766e87b14166
Helder <heldercostaaa@gmail.com>
2020-07-21 21:15:53 -0300

    Add migration to create appointment

---
+++ src/database/migrations/1595376347287-CreateAppointments.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+/**
+ * $ yarn typeorm migration:create -n CreateAppointments
+ * $ yarn typeorm migration:run
+ * $ yarn typeorm migration:revert
+ * $ yarn typeorm migration:show
+ */
+
+export default class CreateAppointments1595376347287
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}

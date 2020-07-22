b7504fd6b34a2238e66adc099fffb7a951ff61b8
Helder <heldercostaaa@gmail.com>
2020-07-22 14:25:27 -0300

    Send JWT token when creating a session

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,12 @@ class AuthenticateUserService {
       throw new Error('Invalid email/password combination');
     }
 
-    return { user };
+    const token = sign({}, '3f7fb3810bbacf0cb269527ef92e5255', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 

ad85a4302228e49ffbfb716293b6ca531edc2ccc
Helder <heldercostaaa@gmail.com>
2020-08-16 17:30:58 -0300

    Test new user creation

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -25,11 +25,11 @@ class UsersRepository implements IUsersRepository {
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
-    const User = this.ormRepository.create(userData);
+    const user = this.ormRepository.create(userData);
 
-    await this.ormRepository.save(User);
+    await this.ormRepository.save(user);
 
-    return User;
+    return user;
   }
 
   public async save(user: User): Promise<User> {

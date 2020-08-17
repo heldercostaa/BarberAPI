dd496e7604d9ce7339f13b5e2c95f153922ff292
Helder <heldercostaaa@gmail.com>
2020-08-16 21:01:35 -0300

    Isolate file storage to providers

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -8,6 +8,7 @@ import AppError from '@shared/errors/AppError';
 
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface IRequest {
   user_id: string;
@@ -19,6 +20,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -29,15 +33,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const filePath = path.join(uploadConfig.directory, user.avatar);
-      const fileExists = await fs.promises.stat(filePath);
-
-      if (fileExists) {
-        await fs.promises.unlink(filePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const fileName = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = fileName;
 
     await this.usersRepository.save(user);
 

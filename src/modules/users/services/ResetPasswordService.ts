49f76d4ee29e0823563f2954c00b86f7d69d831f
Helder <heldercostaaa@gmail.com>
2020-08-20 23:50:06 -0300

    Add ethereal mail provider to test mail on dev

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -21,7 +21,7 @@ class ResetPasswordService {
     @inject('UserTokensRepository')
     private userTokensRepository: IUserTokensRepository,
 
-    @inject('IHashProvider')
+    @inject('HashProvider')
     private hashProvider: IHashProvider,
   ) {}
 
